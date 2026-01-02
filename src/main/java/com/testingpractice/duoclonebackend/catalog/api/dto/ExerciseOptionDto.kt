8df952544857ea806c792a09ed04f93adac1a41d
Dave <davidglogowski2003@gmail.com>
2026-01-02 16:00:19 +0100

    fix(tests): Adjust setup & Jackson mappings

--- src/main/java/com/testingpractice/duoclonebackend/catalog/api/dto/ExerciseOptionDto.kt
+++
@@ -1,9 +0,0 @@
-package com.testingpractice.duoclonebackend.catalog.api.dto
-
-data class ExerciseOptionDto(
-    val id: Int?,
-    val exerciseId: Int?,
-    val content: String?,
-    val imageUrl: String?,
-    val isCorrect: Boolean?
-)
--- src/main/java/com/testingpractice/duoclonebackend/progress/api/dto/NextLessonDto.kt
+++
@@ -1,8 +0,0 @@
-package com.testingpractice.duoclonebackend.progress.api.dto
-
-import com.testingpractice.duoclonebackend.catalog.domain.entity.Lesson
-
-data class NextLessonDto(
-    val nextLesson: Lesson?,
-    val isCourseCompleted: Boolean
-)
